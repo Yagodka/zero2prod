e39733ffef4457b32d24182a7001c531f877cb07
AYagodka <yagodka.andrey@gmail.com>
2022-07-09 09:43:53 +0300

    add subscriptions

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,12 @@ use actix_web::dev::Server;
 use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
 use std::net::TcpListener;
 
+#[derive(serde::Deserialize)]
+struct FormData {
+    name: String,
+    email: String
+}
+
 async fn greet(req: HttpRequest) -> impl Responder {
     let name = req.match_info().get("name").unwrap_or("World");
     format!("Hello {}!", name)
@@ -11,11 +17,18 @@ async fn health_check(_: HttpRequest) -> impl Responder {
     HttpResponse::Ok()
 }
 
+async fn subscribe(form: web::Form<FormData>) -> impl Responder {
+    println!("{}", form.name);
+    println!("{}", form.email);
+    HttpResponse::Ok()
+}
+
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
         App::new()
             .route("/", web::get().to(greet))
             .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
             .route("/{name}", web::get().to(greet))
     })
     .listen(listener)?
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use zero2prod::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind port");
+    let listener = TcpListener::bind("127.0.0.1:8000")
+        .expect("Failed to bind port");
     run(listener)?.await
 }
